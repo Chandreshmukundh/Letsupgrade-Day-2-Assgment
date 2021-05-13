53bbcfdf895941f1ac04729a754e57ac063221c2
Chandresh <83207919+Chandreshmukundh@users.noreply.github.com>
2021-05-13 18:48:40 +0530

    Add files via upload

---
+++ even.py
@@ -0,0 +1,13 @@
+#List with brand names
+
+brand_1 =["acer","hcl","dell","lenovo","asus"]
+
+brand_2 =["microsoft","google","facebook","instagram"]
+
+print(sorted(brand_1))
+print(sorted(brand_2,reverse=True))
+
+
+#print even numbers 20 to 40
+for even in range(20,41,2):
+    print(even,end=" ")
---
+++ overlapping.py
@@ -0,0 +1,9 @@
+#overlapping
+
+list1 =["acer","tesla","hcl","dell","bmw","lenovo","asus","bmw"]
+
+list2 =["microsoft","google","facebook","instagram","tesla","bmw"]
+
+overlapping = set.intersection(set(list1), set(list2))
+
+print(overlapping)
